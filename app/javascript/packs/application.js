5c045d49f49a95c9b0b74c55870338450cfcd322
nova <nova@roadbeats.com>
2020-08-13 09:41:27 +0200

    fix bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
 import 'bootstrap';
 import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/init_select2';
-import { loadDynamicBannerText } from '../components/banner';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { loadDynamicBannerText } from '../components/banner';
 
 
 
@@ -10,8 +10,8 @@ import { initUpdateNavbarOnScroll } from '../components/navbar';
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
   initUpdateNavbarOnScroll();
-  loadDynamicBannerText();
   initSelect2();
+  loadDynamicBannerText();
 });
 
 
