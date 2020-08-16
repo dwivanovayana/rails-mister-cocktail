34247d7c81e06aff2eaf43fc55a0986d3c2888ea
nova <nova@roadbeats.com>
2020-08-16 16:54:03 +0200

    update concktail controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,19 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [ :show, :edit, :update, :destroy]
   def index
     if params[:query].present?
       @cocktails = Cocktail.where("name ILIKE ?", "%#{params[:query]}%")
     else
       @cocktails = Cocktail.all
     end
+  end
 
+
+  def show
+    # @cocktail = Cocktail.find(params[:id])
+    @doses = Dose.where(cocktail: @cocktail)
+    @dose = Dose.new
+    @review = Review.new
   end
 
   def new
@@ -21,20 +29,31 @@ class CocktailsController < ApplicationController
     end
   end
 
-  def show
-    @cocktail = Cocktail.find(params[:id])
-    @doses = Dose.where(cocktail: @cocktail)
-    @dose = Dose.new
-    @review = Review.new
+  def edit
+
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
   end
 
   def destroy
-    @cocktail = Cocktail.find(params[:id])
+    # @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
     redirect_to cocktails_path
   end
 
   private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name, :photo)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'reviews/create'
   root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :new, :show, :create, :destroy] do
+  resources :cocktails do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:create]
   end
